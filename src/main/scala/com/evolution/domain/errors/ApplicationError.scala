6fd262078d62bdcbb71591e73f40326a64ec819d
Anton Padvalnikau <podvalnikov0@gmail.com>
2023-04-13 21:50:01 +0300

    Add error for incorrect team's price

--- src/main/scala/com/evolution/domain/errors/ApplicationError.scala
+++ src/main/scala/com/evolution/domain/errors/ApplicationError.scala
@@ -4,3 +4,4 @@ sealed abstract class ApplicationError(msg: String) extends Throwable(msg)
 
 final case class SuchUserAlreadyExist(msg: String) extends ApplicationError(msg)
 final case object SuchUserDoesNotExist extends ApplicationError("Such user doesn't exist")
+final case object PriseMoreThanBudget extends ApplicationError("Players prise more than your budget")

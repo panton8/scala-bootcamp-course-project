1f2b19db0de0d6b341883bcb30a7287a13b80e45
Anton Padvalnikau <podvalnikov0@gmail.com>
2023-04-18 16:18:50 +0300

    Add queries for player

--- src/main/scala/com/evolution/repository/PlayerRepository.scala
+++ src/main/scala/com/evolution/repository/PlayerRepository.scala
@@ -1,56 +1,235 @@
 package com.evolution.repository
 
-import cats.effect.{ExitCode, IO, IOApp}
+import cats.effect.IO
 import doobie.implicits._
-import UserRepository.{Debugger, xa}
-import com.evolution.domain.{Arsenal, Chelsea, Club, Defender, GamePlace, Goalkeeper, Healthy, Id, Midfielder, Name, Ordinary, Player, Position, Price, Role, Starter, Status, Substituter, Surname}
+import UserRepository.xa
+import com.evolution.domain.Status.{Healthy, Injured}
+import com.evolution.domain.{Club, GameWeek, Id, Name, Player, Position, Price, Statistic, Status, Surname}
 import doobie.implicits.toSqlInterpolator
 
-object PlayerRepository extends IOApp {
+object PlayerRepository{
 
-  def listOfPlayers(): IO[List[Player]] = {
-    val allUsers: fs2.Stream[doobie.ConnectionIO, Player] =
-      fr"select * from players".query[Player].stream
-    allUsers.compile.toList.transact(xa)
-  }
+  def listOfPlayers(): IO[List[Player]] =
+    fr"""
+        SELECT
+            *
+        FROM
+            players
+      """
+      .query[Player]
+      .stream
+      .compile
+      .toList
+      .transact(xa)
 
-  def playerByNameAndSurname(name: Name, surname: Surname): IO[Option[Player]] = {
-    fr"select * from players where name=${name.value} and surname=${surname.value}".query[Player].option.transact(xa)
-  }
+  def playerByNameAndSurname(name: Name, surname: Surname): IO[Option[Player]] =
+    fr"""
+        SELECT
+            *
+        FROM
+            players
+        WHERE
+            name = ${ name.value }
+            AND surname = ${ surname.value }
+      """
+      .query[Player]
+      .option
+      .transact(xa)
 
-  def playerById(id: Id): IO[Option[Player]] = {
-    fr"select * from players where id=${id.value}".query[Player].option.transact(xa)
-  }
+  def playerById(id: Id): IO[Option[Player]] =
+    fr"""
+        SELECT
+            *
+        FROM
+            players
+        WHERE
+            id = ${  id.value  }
+      """
+      .query[Player]
+      .option
+      .transact(xa)
 
-  def playersByClub(club: Club): IO[List[Player]] = {
-    fr"select * from players where club=${club.toString}".query[Player].stream.compile.toList.transact(xa)
-  }
+  def playersByClub(club: Club): IO[List[Player]] =
+    fr"""
+        SELECT
+            *
+        FROM
+            players
+        WHERE
+            club = $club
+      """
+      .query[Player]
+      .stream
+      .compile
+      .toList
+      .transact(xa)
 
-  def playersByPosition(position: Position): IO[List[Player]] = {
-    fr"select * from players where pos=${position.toString}".query[Player].stream.compile.toList.transact(xa)
-  }
+  def playersByPosition(position: Position): IO[List[Player]] =
+    fr"""
+        SELECT
+            *
+        FROM
+            players
+        WHERE
+            pos = $position
+      """
+      .query[Player]
+      .stream
+      .compile
+      .toList
+      .transact(xa)
 
-  def getInjured(name: Name, surname: Surname): IO[Int] = fr"update players set health_status = ${"Injured"} where name=${name.value} and surname=${surname.value}".update.run.transact(xa)
+  def getInjured(playerId: Id): IO[Int] =
+    fr"""
+        UPDATE
+            players
+        SET
+            health_status = ${Injured.entryName}
+        WHERE
+            id = ${ playerId.value }
+      """
+      .update
+      .run
+      .transact(xa)
 
-  def getRecovered(name: Name, surname: Surname): IO[Int] = fr"update players set health_status = ${"Healthy"} where name=${name.value} and surname=${surname.value}".update.run.transact(xa)
+  def getRecovered(playerId: Id): IO[Int] =
+    fr"""
+        UPDATE
+            players
+        SET
+            health_status = ${Healthy.entryName}
+        WHERE
+            id = ${ playerId.value }
+      """
+      .update
+      .run
+      .transact(xa)
 
-  def changePrice(name: Name, surname: Surname, price: Price): IO[Int] = fr"update players set price=${price.value} where name=${name.value} and surname=${surname.value}".update.run.transact(xa)
+  def changePrice(playerId: Id, price: Price): IO[Int] =
+    fr"""
+        UPDATE
+            players
+        SET
+            price = ${ price.value }
+        WHERE
+            id = ${ playerId.value }
+      """
+      .update
+      .run
+      .transact(xa)
 
-  def changeClub(name: Name, surname: Surname, club: Club): IO[Int] = fr"update players set club=${club.toString} where name=${name.value} and surname=${surname.value}".update.run.transact(xa)
+  def changeClub(playerId: Id, club: Club): IO[Int] =
+    fr"""
+        UPDATE
+            players
+        SET
+            club = $club
+        WHERE
+            id = ${ playerId.value }
+      """
+      .update
+      .run
+      .transact(xa)
 
-  def changeGamePlace(name: Name, surname: Surname): IO[Int] = fr"update players set game_place = case when game_place=${"Starter"} then ${"Substituter"} else ${"Starter"} end where name=${name.value} and surname=${surname.value}".update.run.transact(xa)
+  def updateStatistics(statistic: Statistic, player: Id): IO[Option[Int]] =
+    fr"""
+        INSERT INTO
+            statistics (
+                game_week,
+                goals,
+                assists,
+                minutes,
+                own_goals,
+                yellow_cards,
+                red_cards,
+                saves,
+                clean_sheet,
+                player_id
+             )
+        VALUES
+            (
+               ${statistic.gameWeek.value},
+               ${statistic.goals},
+               ${statistic.assists},
+               ${statistic.minutes},
+               ${statistic.ownGoals},
+               ${statistic.yellowCard},
+               ${statistic.redCard},
+               ${statistic.saves},
+               ${statistic.cleanSheet},
+               ${player.value}
+            )
+      """
+      .update
+      .run
+      .transact(xa)
+      .option
 
-  def setCaptain(name: Name, surname: Surname): IO[Int] = fr"update players set role=${"Captain"} where name=${name.value} and surname=${surname.value}".update.run.transact(xa)
+  def showPlayerStatisticsByWeek(playerId: Id, gameWeek: GameWeek): IO[Statistic] =
+    fr"""
+        SELECT
+            game_week,
+            goals,
+            assists,
+            minutes,
+            own_goals,
+            yellow_cards,
+            red_cards,
+            saves,
+            clean_sheet
+        FROM
+            statistics
+        WHERE
+            player_id = ${ playerId.value }
+            AND game_week = ${ gameWeek.value }
+      """
+      .query[Statistic]
+      .unique
+      .transact(xa)
 
-  def addPlayer(name: Name, surname: Surname, price: Price, club: Club, position: Position, role: Role, status: Status, gamePlace: GamePlace):IO[Int] = {
-    fr"insert into players (name, surname, club, price, pos, role, health_status, game_place) values (${name.value}, ${surname.value}, ${club.toString},${price.value}, ${position.toString}, ${role.toString}, ${status.toString}, ${gamePlace.toString} )".
-        update.withUniqueGeneratedKeys[Int]("id").transact(xa)
-  }
+  def showTotalPlayerStatistics(playerId: Id):IO[Statistic] =
+    fr"""
+        SELECT
+            MAX(game_week),
+            SUM(goals),
+            SUM(assists),
+            SUM(minutes),
+            SUM(own_goals),
+            SUM(yellow_cards),
+            SUM(red_cards),
+            SUM(saves),
+            SUM(clean_sheet)
+        FROM
+            statistics
+        WHERE
+            player_id = ${ playerId.value }
+      """
+      .query[Statistic]
+      .unique
+      .transact(xa)
 
-
-  override def run(args: List[String]): IO[ExitCode] = {
-    playersByPosition(Defender).debug.as(ExitCode.Success)
-    //addPlayer(Name("Andrew"), Surname("Sivy"), Price(0.1), AA, Goalkeeper, Ordinary, Healthy, Substituter).debug.as(ExitCode.Success)
-    //addUser(Name("jexommo_12"), Email("jeiniproxommo7458@yopmail.com"), Password("01042023"), Base, Budget(100)).debug.as(ExitCode.Success)
-  }
+  def addPlayer(name: Name, surname: Surname, price: Price, club: Club, position: Position, status: Status): IO[Int] =
+    fr"""
+        INSERT INTO
+             players (
+                 name,
+                 surname,
+                 club,
+                 price,
+                 pos,
+                 health_status,
+                 game_place
+             )
+    VALUES
+        (
+            ${ name.value },
+            ${ surname.value },
+            ${ club.toString },
+            ${ price.value },
+            ${ position.entryName },
+            ${ status }
+        )
+      """
+      .update.withUniqueGeneratedKeys[Int]("id")
+      .transact(xa)
 }

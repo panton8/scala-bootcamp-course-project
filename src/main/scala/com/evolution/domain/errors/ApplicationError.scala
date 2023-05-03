0fedc15317eab0b2dbe19a412de24b78bdd8e5a0
Anton Padvalnikau <podvalnikov0@gmail.com>
2023-05-03 23:25:32 +0300

    Add error for authentication

--- src/main/scala/com/evolution/domain/errors/ApplicationError.scala
+++ src/main/scala/com/evolution/domain/errors/ApplicationError.scala
@@ -1,8 +1,19 @@
 package com.evolution.domain.errors
 
+import io.circe.{Decoder, Encoder}
+
 sealed abstract class ApplicationError(msg: String) extends Throwable(msg)
 
 final case class SuchUserAlreadyExist(msg: String) extends ApplicationError(msg)
 final case object SuchUserDoesNotExist extends ApplicationError("Such user doesn't exist")
 final case object PriseMoreThanBudget extends ApplicationError("Players prise more than your budget")
 final case object NonAvailableTransfer extends ApplicationError("No available transfers")
+final case class AuthenticationError(msg: String) extends ApplicationError(msg)
+
+object AuthenticationError {
+  implicit val userRegDecoder: Decoder[AuthenticationError] =
+    Decoder.forProduct1("error")(AuthenticationError.apply)
+
+  implicit val userRegEncoder: Encoder[AuthenticationError] =
+    Encoder.forProduct1("error")(e => ge.msg)
+}

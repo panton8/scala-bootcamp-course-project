1af38e8697b2e660e047b9a99da265f8c7d98b2d
Anton Padvalnikau <podvalnikov0@gmail.com>
2023-05-07 01:59:34 +0300

    Add routes for team

--- src/main/scala/com/evolution/http/routes/TeamRoutes.scala
+++ src/main/scala/com/evolution/http/routes/TeamRoutes.scala
@@ -1,14 +1,12 @@
 package com.evolution.http.routes
 
-import cats._
 import cats.effect._
-import cats.implicits._
-import com.evolution.domain.{Id, Team}
+import com.evolution.domain.errors.SuchTeamDoesNotExist
+import com.evolution.domain.{GameWeek, Id, Player}
+import com.evolution.http.domain.TeamCreation
 import com.evolution.service.TeamService
-import org.http4s.circe._
 import org.http4s._
-import io.circe.generic.auto._
-import io.circe.syntax._
+import org.http4s.circe.CirceEntityCodec._
 import org.http4s.dsl._
 
 object TeamRoutes {
@@ -19,30 +17,42 @@ object TeamRoutes {
 
     HttpRoutes.of[IO] {
       case GET -> Root / "teams" =>
-        teamService.showListOfTeams().flatMap(teams => Ok(teams.asJson))
+        teamService.showListOfTeams().flatMap(teams => Ok(teams))
 
       case GET -> Root / "teams" / IntVar(id) =>
         teamService.findById(Id(id)) flatMap {
-          case None => NotFound()
-          case Some(team) => Ok(team.asJson)
-        }
-
-      case req @ POST -> Root / "teams" =>
-        req.decode[Team] { team =>
-          teamService.createTeam(team.name, user = ???, team.players, captain = ???)
-        }
-
-      case GET -> Root / "teams" / IntVar(id) =>
-        teamService.findById(Id(id)) flatMap {
+          case Some(team) => Ok(team)
           case None       => NotFound()
-          case Some(team) => Ok(team.asJson)
         }
 
-      case req @ PUT -> Root / "teams" / id =>
-        teamService.updateTeamStat(team =  ???, currentWeek =  ???)
+      case GET -> Root / "teams" / IntVar(id) / "line-up" =>
+        teamService.playersFromTeam(Id(id)) flatMap {players =>
+          if (players.isEmpty)
+            NotFound(SuchTeamDoesNotExist.getMessage)
+          else
+            Ok(players)
+        }
 
-      case req @ PUT -> Root / "teams" / id =>
-        teamService.changePlayer(newPlayer = ???, subPlayer = ???, team = ???, user = ???)
+      case req @ PUT -> Root / "teams" / IntVar(id) / "update" / "captain" =>
+        for {
+          newCaptain <- req.as[Player]
+          response   <- Ok(teamService.resetCaptain(Id(id), newCaptain))
+        } yield response
+
+      case PUT -> Root / "teams" / IntVar(id) / "matchweek" / IntVar(currentWeek) / "update" / "points" =>
+        teamService.updateTeamStat(Id(id),GameWeek(currentWeek)) flatMap(_ => Ok().handleErrorWith(e => BadRequest(e.getMessage)))
+
+      case req @ PUT -> Root / "teams" / IntVar(teamId) / "update" / "line-up" =>
+        for {
+          players  <- req.as[List[Player]]
+          response <- Ok(teamService.changePlayer(players(1), players.head,Id(teamId))).handleErrorWith(e => BadRequest(e.getMessage))
+        } yield response
+
+      case req @ POST -> Root / "users" / IntVar(id) / "teams" =>
+        for {
+          team     <- req.as[TeamCreation]
+          response <- Ok(teamService.createTeam(team.name, Id(id), team.players, team.captain)).handleErrorWith(e => BadRequest(e.getMessage))
+        } yield response
     }
   }
 }

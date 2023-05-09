42766743998416e2c38ea5b3e4e14aa44b2b770b
Anton Padvalnikau <podvalnikov0@gmail.com>
2023-05-10 01:03:10 +0300

    Add codecs

--- src/main/scala/com/evolution/domain/Email.scala
+++ src/main/scala/com/evolution/domain/Email.scala
@@ -1,3 +1,19 @@
 package com.evolution.domain
 
+import io.circe.syntax.EncoderOps
+import io.circe.{Decoder, Encoder, Json}
+
 final case class Email(value: String) extends AnyVal
+
+object Email {
+  implicit val jsonDecoder: Decoder[Email] = cursor =>
+    for {
+      email <- cursor.get[String]("email")
+    } yield Email(email)
+
+  implicit val jsonEncoder: Encoder[Email] = Encoder.instance {
+    case Email(email) => Json.obj(
+      "email" -> email.asJson,
+    )
+  }
+}

33b055b70601dc28559ba8ce7c8fca4386070c10
Anton Padvalnikau <podvalnikov0@gmail.com>
2023-05-12 00:54:12 +0300

    Update server setup

--- src/main/scala/com/evolution/http/AppServer.scala
+++ src/main/scala/com/evolution/http/AppServer.scala
@@ -4,20 +4,16 @@ import cats.effect.{ExitCode, IO, IOApp}
 import org.http4s.server.Router
 import org.http4s.implicits._
 import com.evolution.http.routes.{PlayerRoutes, TeamRoutes, UserRoutes}
-import com.evolution.service.{PlayerService, TeamService, UserService}
+
 import org.http4s.ember.server.EmberServerBuilder
 
 
 object AppServer extends IOApp {
 
-  private val userService: UserService = UserService()
-  private val teamService: TeamService = TeamService()
-  private val playerService: PlayerService = PlayerService()
-
-  val httpRoutes = Router[IO](
-    "/" -> UserRoutes.routes(userService),
-    "/" -> TeamRoutes.routes(teamService),
-    "/" -> PlayerRoutes.routes(playerService)
+  private val httpRoutes = Router[IO](
+    "/" -> UserRoutes.service,
+    "/" -> TeamRoutes.service,
+    "/" -> PlayerRoutes.service
   ).orNotFound
 
   override def run(args: List[String]): IO[ExitCode] = {

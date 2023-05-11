f896e7ddd625cb14d128f23d4f714d999de3ad9a
Anton Padvalnikau <podvalnikov0@gmail.com>
2023-05-12 00:49:18 +0300

    Add new error

--- src/main/scala/com/evolution/domain/errors/ApplicationError.scala
+++ src/main/scala/com/evolution/domain/errors/ApplicationError.scala
@@ -6,6 +6,7 @@ sealed abstract class ApplicationError(msg: String) extends Throwable(msg)
 
 final case class SuchUserAlreadyExist(msg: String) extends ApplicationError(msg)
 final case object SuchUserDoesNotExist extends ApplicationError("Such user doesn't exist")
+final case object SuchTeamDoesNotExist extends ApplicationError("Such team doesn't exist")
 final case object PriseMoreThanBudget extends ApplicationError("Players prise more than your budget")
 final case object NonAvailableTransfer extends ApplicationError("No available transfers")
 final case class AuthenticationError(msg: String) extends ApplicationError(msg)

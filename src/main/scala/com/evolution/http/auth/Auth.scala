3575923c7d1b3c6f794b17f51a742de4c46d09ea
Anton Padvalnikau <podvalnikov0@gmail.com>
2023-05-14 03:21:22 +0300

    Replace errors

--- src/main/scala/com/evolution/http/auth/Auth.scala
+++ src/main/scala/com/evolution/http/auth/Auth.scala
@@ -5,8 +5,6 @@ import cats.effect.IO
 import com.evolution.domain.errors.AuthenticationError
 import com.evolution.domain.{Email, Password, User}
 import com.evolution.service.UserService
-import org.http4s.circe.CirceEntityCodec.{circeEntityDecoder, circeEntityEncoder}
-import org.http4s.dsl.io._
 import org.http4s.headers.Authorization
 import org.http4s.{AuthedRequest, _}
 import org.http4s.implicits.http4sHeaderSyntax
@@ -21,13 +19,20 @@ object Auth {
 
   def authUser: Kleisli[IO, Request[IO], Either[AuthenticationError, User]] = Kleisli { request: Request[IO] =>
     request.headers.get[Authorization] match {
-      case Some(h) => getAuthUser(h.value).map(_.toRight(AuthenticationError("Access denied")))
-      case None    => IO.pure(Left(AuthenticationError("Access denied")))
+      case Some(h) => getAuthUser(h.value).map(_.toRight(AuthenticationError("Incorrect password or email")))
+      case None    => IO.pure(Left(AuthenticationError("Incorrect password or email")))
     }
   }
 
-  def inAuthFailure: AuthedRoutes[AuthenticationError, IO] = Kleisli { request: AuthedRequest[IO, AuthenticationError] =>
-    OptionT.liftF(Forbidden(request.context))
+  def inAuthFailure: AuthedRoutes[AuthenticationError, IO] = Kleisli { req: AuthedRequest[IO, AuthenticationError] =>
+    req.req match {
+      case _ =>
+        OptionT.pure[IO](
+          Response[IO](
+          status = Status.Unauthorized
+        )
+      )
+    }
   }
 
   def authMiddleware: AuthMiddleware[IO, User] = AuthMiddleware(authUser, inAuthFailure)

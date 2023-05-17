6ed4d374d98801ef2c7702edce0d04e4bd0c18ae
Anton Padvalnikau <podvalnikov0@gmail.com>
2023-05-17 11:53:47 +0300

    Add codecs for id

--- src/main/scala/com/evolution/domain/Id.scala
+++ src/main/scala/com/evolution/domain/Id.scala
@@ -1,3 +1,19 @@
 package com.evolution.domain
 
+import io.circe.syntax.EncoderOps
+import io.circe.{Decoder, Encoder, Json}
+
 final case class Id(value: Int) extends AnyVal
+
+object Id {
+  implicit val jsonDecoder: Decoder[Id] = cursor =>
+    for {
+      id <- cursor.get[Int]("id")
+    } yield Id(id)
+
+  implicit val jsonEncoder: Encoder[Id] = Encoder.instance {
+    case Id(id) => Json.obj(
+      "id" -> id.asJson,
+    )
+  }
+}

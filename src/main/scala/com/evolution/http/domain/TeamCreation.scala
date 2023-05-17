f6b78dadae7280fd068cf4f90235f30b303f67a9
Anton Padvalnikau <podvalnikov0@gmail.com>
2023-05-17 11:56:26 +0300

    Update codecs

--- src/main/scala/com/evolution/http/domain/TeamCreation.scala
+++ src/main/scala/com/evolution/http/domain/TeamCreation.scala
@@ -1,17 +1,17 @@
 package com.evolution.http.domain
 
-import com.evolution.domain.{Name, Player}
+import com.evolution.domain.{Id, Name}
 import io.circe.syntax.EncoderOps
 import io.circe.{Decoder, Encoder, Json}
 
-final case class TeamCreation(name: Name, players: List[Player], captain: Player)
+final case class TeamCreation(name: Name, players: List[Id], captain: Id)
 
 object TeamCreation {
   implicit val jsonDecoder: Decoder[TeamCreation] = cursor =>
   for {
     name    <- cursor.get[String]("name")
-    players <- cursor.get[List[Player]]("players")
-    captain <- cursor.get[Player]("captain")
+    players <- cursor.get[List[Id]]("players")
+    captain <- cursor.get[Id]("captain")
   }  yield TeamCreation(Name(name), players, captain)
 
   implicit val jsonEncoder: Encoder[TeamCreation] = Encoder.instance {

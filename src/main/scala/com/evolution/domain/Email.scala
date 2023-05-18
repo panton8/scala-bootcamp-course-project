ce6e93d6ddf1108946785e4f6fc0ba15515150c2
Anton Padvalnikau <podvalnikov0@gmail.com>
2023-05-18 16:46:47 +0300

    Add email validation

--- src/main/scala/com/evolution/domain/Email.scala
+++ src/main/scala/com/evolution/domain/Email.scala
@@ -1,19 +1,32 @@
 package com.evolution.domain
 
+import com.evolution.domain.errors.SuchUserDoesNotExist
+import io.circe.Decoder.Result
 import io.circe.syntax.EncoderOps
-import io.circe.{Decoder, Encoder, Json}
+import io.circe.{Decoder, DecodingFailure, Encoder, HCursor, Json}
 
-final case class Email(value: String) extends AnyVal
+final case class Email (value: String) extends AnyVal
 
 object Email {
-  implicit val jsonDecoder: Decoder[Email] = cursor =>
-    for {
-      email <- cursor.get[String]("email")
-    } yield Email(email)
 
-  implicit val jsonEncoder: Encoder[Email] = Encoder.instance {
-    case Email(email) => Json.obj(
-      "email" -> email.asJson,
-    )
+  /*def apply(email: String): Option[Email] =
+    if (email.matches("^\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$"))
+      Some(new Email(email))
+    else None*/
+
+  implicit val jsonDecoder: Decoder[Email] = new Decoder[Email] {
+    override def apply(cursor: HCursor): Result[Email] = cursor.get[String]("email") match {
+      case Left(_) => Left(DecodingFailure("Error", cursor.history))
+      case Right(email) => if (email.matches("^\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$"))
+        Right(Email(email))
+      else
+        Left(DecodingFailure("Error", cursor.history))
+    }
   }
+
+    implicit val jsonEncoder: Encoder[Email] = Encoder.instance {
+      case Email(email) => Json.obj(
+        "email" -> email.asJson,
+      )
+    }
 }

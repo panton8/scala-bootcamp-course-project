30d85dc94ed30c66015a45ce1d95f19b86269e5f
Anton Padvalnikau <podvalnikov0@gmail.com>
2023-05-20 21:52:17 +0300

    Add registration validator

---
+++ src/main/scala/com/evolution/service/domain/RegistrationValidator.scala
@@ -0,0 +1,17 @@
+package com.evolution.service.domain
+
+import cats.data.ValidatedNec
+import cats.effect.IO
+import cats.implicits.catsSyntaxTuple3Semigroupal
+import com.evolution.domain.errors.ApplicationError
+import com.evolution.domain.{Email, Name, Password}
+import com.evolution.http.domain.UserRegistration
+
+object RegistrationValidator {
+  def validate(userName: String, email: String, password: String): IO[ValidatedNec[ApplicationError, UserRegistration]] = {
+    IO.pure((Name.fromString(userName),
+      Email.fromString(email),
+      Password.fromString(password)
+    ).mapN(UserRegistration.apply))
+  }
+}

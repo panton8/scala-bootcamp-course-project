264279e38f806f45e6eeb5c1bb9541bc11b9450a
Anton Padvalnikau <podvalnikov0@gmail.com>
2023-05-20 20:41:28 +0300

    Add validation for registration

--- src/main/scala/com/evolution/domain/Email.scala
+++ src/main/scala/com/evolution/domain/Email.scala
@@ -1,28 +1,30 @@
 package com.evolution.domain
 
-import com.evolution.domain.errors.SuchUserDoesNotExist
-import io.circe.Decoder.Result
+import cats.data.{Validated, ValidatedNec}
+import cats.implicits.catsSyntaxApply
+import com.evolution.domain.errors.{ApplicationError, InvalidEmail}
 import io.circe.syntax.EncoderOps
-import io.circe.{Decoder, DecodingFailure, Encoder, HCursor, Json}
+import io.circe.{Decoder, Encoder, Json}
 
 final case class Email (value: String) extends AnyVal
 
 object Email {
 
-  /*def apply(email: String): Option[Email] =
-    if (email.matches("^\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$"))
-      Some(new Email(email))
-    else None*/
+  def fromString(email: String): ValidatedNec[ApplicationError, Email] = {
+    def isEmpty:ValidatedNec[ApplicationError, String] =
+      Validated.cond(!email.isEmpty, email, InvalidEmail("Password can't be blank")).toValidatedNec
 
-  implicit val jsonDecoder: Decoder[Email] = new Decoder[Email] {
-    override def apply(cursor: HCursor): Result[Email] = cursor.get[String]("email") match {
-      case Left(_) => Left(DecodingFailure("Error", cursor.history))
-      case Right(email) => if (email.matches("^\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$"))
-        Right(Email(email))
-      else
-        Left(DecodingFailure("Error", cursor.history))
+    def correctSymbols: ValidatedNec[ApplicationError, String] = {
+      Validated.cond(email.matches("^\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$") |
+        email.isEmpty, email, InvalidEmail("Invalid email")).toValidatedNec
     }
+    (isEmpty *> correctSymbols).map(email => Email(email))
   }
+
+  implicit val jsonDecoder: Decoder[Email] = cursor =>
+    for {
+      email <- cursor.get[String]("email")
+    } yield Email(email)
 
     implicit val jsonEncoder: Encoder[Email] = Encoder.instance {
       case Email(email) => Json.obj(
